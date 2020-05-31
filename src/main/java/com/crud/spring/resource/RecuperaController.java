b24a1f6992e9bc6353441a8f5d189bd06e0a943b
unknown <vargas.reynaldo22@gmail.com>
2020-05-30 20:18:50 -0400

    implementacao de recuperacao de senha de acesso

--- src/main/java/com/crud/spring/resource/RecuperaController.java
+++ src/main/java/com/crud/spring/resource/RecuperaController.java
@@ -1,8 +1,12 @@
 package com.crud.spring.resource;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.crud.spring.ObjetoError;
 import com.crud.spring.model.Usuario;
 import com.crud.spring.repository.UsuarioRepository;
+import com.crud.spring.service.EmailService;
 
 @RestController
 @RequestMapping("/recuperar")
@@ -20,9 +25,12 @@ public class RecuperaController {
 	@Autowired
 	private UsuarioRepository usuarioRepo;
 
+	@Autowired
+	private EmailService emailservice;
+	
 	@ResponseBody
 	@PostMapping(value = "/")
-	public ResponseEntity<ObjetoError> recuperar(@RequestBody Usuario login) {
+	public ResponseEntity<ObjetoError> recuperar(@RequestBody Usuario login) throws Exception {
 		ObjetoError objetoError = new ObjetoError();
 
 		Usuario user = usuarioRepo.findUserByLogin(login.getLogin());
@@ -31,10 +39,19 @@ public class RecuperaController {
 			objetoError.setCode("404");
 			objetoError.setError("Usuário não encontrado");
 		} else {
+			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+			String senhaNova = dateFormat.format(Calendar.getInstance().getTime());
+			String senhaNovaCriptografada = new BCryptPasswordEncoder().encode(senhaNova);
+			
+			usuarioRepo.updateSenha(senhaNovaCriptografada, user.getId());
+			
+			emailservice.enviarEmail("Recuperação se senha", user.getLogin(),
+					"Sua nova senha é: " + senhaNova);
+			
 			objetoError.setCode("200");
-			objetoError.setError("Acesso enciado para o seu e-mail");
+			objetoError.setError("Acesso enviado para o seu e-mail");
 		}
-		return new ResponseEntity<ObjetoError>(objetoError, HttpStatus.OK);
+		return new ResponseEntity<>(objetoError, HttpStatus.OK);
 	}
 
 }
